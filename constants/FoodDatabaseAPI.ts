7d41ab599b4d4a7ae29adaa33cb36f6356881d9c
Storm Tuchek <satuchek@gmail.com>
2024-07-15 17:39:44 -0400

    Added StepZen Configuration

---
+++ constants/FoodDatabaseAPI.ts
@@ -0,0 +1,5 @@
+export const foodAPIKey = '5e36f859d87c1618fc4983acf78c28d9';
+export const foodAPIAppId = '28c5d6e7';
+export const sampleQuery = "curl -X 'GET' \
+  'https://api.edamam.com/api/food-database/v2/parser?app_id=28c5d6e7&app_key=5e36f859d87c1618fc4983acf78c28d9&ingr=potato&nutrition-type=cooking' \
+  -H 'accept: application/json'"
---
+++ constants/IBMStepZen.ts
@@ -0,0 +1,4 @@
+export const adminKey = 'khosuyeh::local.io+1000::01392324b79c28de4d596dba61f401452c93f5d795c64a3c5aec82ddda085f0b';
+export const apiKey = 'khosuyeh::local.net+1000::5b06f1a37b62d3969c4096685f091150086e4eec37b95819fba15921ec06fc3f';
+export const environment = 'khosuyeh';
+export const domain = 'us-east-a.ibm.stepzen.net';
