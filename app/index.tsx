4149fc5f377b700bbc65c876ce97730c01704659
Alekya T <alekya.thalakoti@mga.edu>
2024-07-18 13:13:52 -0400

    Added Scan Barcode Functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,60 +1,79 @@
-import FoodListItem from '@/components/FoodListItem';
 import { Link } from 'expo-router'
 import React from 'react'
-import { View, Text, FlatList, Button, StyleSheet, Pressable } from 'react-native'
+import { View, Text, FlatList, Button, StyleSheet, Pressable, ActivityIndicator } from 'react-native'
+import { gql, useQuery } from '@apollo/client';
+import dayjs from 'dayjs';
+import FoodLogListItem from '@/components/FoodLogListItem';
 
+const query = gql`
+query foodLogsForDate($date: Date!, $user_id: String!) {
+  foodLogsForDate(date: $date, user_id: $user_id) {
+    food_id
+    created_at
+    user_id
+    id
+    kcal
+    label
+  }
+}
+  `;
 
-const foodItems = [
-    { 
-        food: { label: "Pizza", nutrients: { ENERC_KCAL: 70}, brand: "Dominos"} 
-    },
-    { food: {label: "Calzone", nutrients: {ENERC_KCAL: 250}, brand: "2 Guy's and a Pie"} },
-    { food: {label: "Pepperoni Slice", nutrients: {ENERC_KCAL:180}, brand: "Marcos"} },
-    { food: {label: "Sushi", nutrients: {ENERC_KCAL: 50}, brand: "Sushi Place" }},
-  ];
-  
 export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-        <View style={styles.headerRow}>
-            <Text style={styles.subtitle}>Calories</Text>
-            <Text style={{}}>1770 - 360 = 1692</Text>
-        </View>
-        <View style={styles.headerRow}>
-            <Text style={styles.subtitle}>Today's Logged Food</Text>
-            {/* This is different from the example, but <Button> now throws an error within React Native when nested under a Link. */}
-            <Link href="/search" asChild>
-                <Pressable>
-                    <Text style={{color: 'royalblue', fontSize: 18, fontWeight:'bold'}}>ADD FOOD</Text>
-                </Pressable>
-            </Link>
-        </View>
-        <FlatList
-            data={foodItems}
-            contentContainerStyle={{gap: 5}}
-            renderItem={({item}) => <FoodListItem item={item} />}
-        />
+    const user_id = 'Bob';
+    const { data, loading, error } = useQuery(query,
+        {
+            variables: {
+                date: dayjs().format('YYYY-MM-DD'),
+                user_id,
+            },
+        });
+    if (loading) {
+        return <ActivityIndicator />;
+    }
+    if (error) {
+        return <Text>Failed to fetch data</Text>;
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.headerRow}>
+                <Text style={styles.subtitle}>Calories</Text>
+                <Text style={{}}>1770 - 360 = 1692</Text>
+            </View>
+            <View style={styles.headerRow}>
+                <Text style={styles.subtitle}>Today's Logged Food</Text>
+                {/* This is different from the example, but <Button> now throws an error within React Native when nested under a Link. */}
+                <Link href="/search" asChild>
+                    <Pressable>
+                        <Text style={{ color: 'royalblue', fontSize: 18, fontWeight: 'bold' }}>ADD FOOD</Text>
+                    </Pressable>
+                </Link>
+            </View>
+            <FlatList
+                data={data.foodLogsForDate}
+                contentContainerStyle={{ gap: 5 }}
+                renderItem={({ item }) => <FoodLogListItem item={item} />}
+            />
 
-    </View>
-  )
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'white', 
-        flex: 1, 
-        padding: 10, 
+        backgroundColor: 'white',
+        flex: 1,
+        padding: 10,
         gap: 10
     },
     headerRow: {
-        flexDirection: 'row', 
-        justifyContent: 'space-between', 
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         alignItems: 'center'
     },
-    subtitle: { 
-        fontSize: 18, 
-        fontWeight: '500', 
-        flex: 1, 
+    subtitle: {
+        fontSize: 18,
+        fontWeight: '500',
+        flex: 1,
         color: 'dimgray'
-    }
-})
+    },
+});
--- app/search.tsx
+++ app/search.tsx
@@ -8,58 +8,106 @@ import {
   ActivityIndicator,
 } from "react-native";
 import FoodListItem from "@/components/FoodListItem";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { gql, useLazyQuery } from '@apollo/client';
 import React from "react";
+import { Ionicons } from '@expo/vector-icons';
+// import { Camera } from 'expo-camera';
+import { Camera, CameraType } from 'expo-camera/legacy';
 
-const query = gql `
-  query MyQuery($ingr: String) {
-    search(ingr: $ingr) {
-      text
-      hints {
-        food {
-          label
-          brand
-          foodId
-          nutrients {
-            ENERC_KCAL
-          }
-        }
+
+const query = gql`
+  query MyQuery($ingr: String, $upc: String) {
+  search(ingr: $ingr, upc: $upc) {
+    text
+    hints {
+    food {
+      label
+      brand
+      foodId
+      nutrients {
+      ENERC_KCAL
       }
     }
+    }
   }
-`
+  } `
+
+
 
 
 export default function SearchScreen() {
   const [search, setSearch] = useState("");
+  const [scannerEnabled, setScannerEnabled] = useState(false);
 
-  const [runSearch, { data, loading, error }] = useLazyQuery(query, {variables: {ingr: search}});
+  const [runSearch, { data, loading, error }] = useLazyQuery(query, { variables: { ingr: search } });
+  // const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  // requestPermission();
 
   const performSearch = () => {
-    runSearch({variables: {ingr: search}});
- 
-  };
+    runSearch({ variables: { ingr: search } });
 
+  };
 
   if (error) {
     return <Text>Failed to Search</Text>;
+  }
+
+  if (scannerEnabled) {
+    if (hasPermission === null) {
+      return <View />;
+    }
+    if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+
+    return (
+      <View style={{ flex: 1 }}>
+        <Camera
+          style={{ width: '100%', height: '100%' }} onBarCodeScanned={(data) => {
+            console.log(data);
+            runSearch({ variables: { upc: data.data } });
+            setScannerEnabled(false);
+          }}
+        />
+        <Ionicons
+          onPress={() => setScannerEnabled(false)}
+          name="close"
+          size={30}
+          color="dimgray"
+          style={{ position: 'absolute', right: 10, top: 10 }}
+        />
+      </View>
+    );
   }
 
   const items = data?.search?.hints || [];
 
   return (
     <View style={styles.container}>
-      <TextInput
-        value={search}
-        onChangeText={setSearch}
-        placeholder="Search . . ."
-        style={styles.input}
-      />
+      <View style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}>
+        <TextInput
+          value={search}
+          onChangeText={setSearch}
+          placeholder="Search . . ."
+          style={styles.input}
+        />
 
+        <Ionicons onPress={() => setScannerEnabled(true)}
+          name="barcode-outline" size={32} color="dimgray" />
+      </View>
       {search && <Button title="Search" onPress={performSearch} />}
 
-      { loading && <ActivityIndicator />}
+      {loading && <ActivityIndicator />}
       <FlatList
         data={items}
         renderItem={({ item }) => <FoodListItem item={item} />}
@@ -81,7 +129,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#f2f2f2",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
   },
 });
 
+function setHasPermission(arg0: boolean) {
+  throw new Error("Function not implemented.");
+}
 
